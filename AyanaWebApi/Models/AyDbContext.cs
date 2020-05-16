73e6f7df071502106b368972c4cd68ef92f983ea
Efanov P.A <efanov.pa.a@gmail.com>
2020-05-16 21:25:53 +0300

    Начальная миграция бд

---
+++ AyanaWebApi/Models/AyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AyanaWebApi.Models
+{
+    public class AyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Initial Catalog=AyanaDB;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ AyanaWebApi/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AyanaWebApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Token { get; set; }
+    }
+}
