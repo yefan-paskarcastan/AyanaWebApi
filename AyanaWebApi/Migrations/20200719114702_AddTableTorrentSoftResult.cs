01d67a8f32e4f9366c11356941962009ef0edcf3
Efanov P.A <efanov.pa.a@gmail.com>
2020-07-19 14:48:46 +0300

    Добавлена таблица для результатов отправки постов

---
+++ AyanaWebApi/Migrations/20200719114702_AddTableTorrentSoftResult.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AyanaWebApi.Migrations
+{
+    public partial class AddTableTorrentSoftResult : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TorrentSoftResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Created = table.Column<DateTime>(nullable: false),
+                    TorrentSoftPostId = table.Column<int>(nullable: false),
+                    SendPostIsSuccess = table.Column<bool>(nullable: false),
+                    TorrentFileIsSuccess = table.Column<bool>(nullable: false),
+                    PosterIsSuccess = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TorrentSoftResults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TorrentSoftResults_TorrentSoftPosts_TorrentSoftPostId",
+                        column: x => x.TorrentSoftPostId,
+                        principalTable: "TorrentSoftPosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TorrentSoftResults_TorrentSoftPostId",
+                table: "TorrentSoftResults",
+                column: "TorrentSoftPostId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TorrentSoftResults");
+        }
+    }
+}
