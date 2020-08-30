8d520c7966ffa90f675bdb4c993dea72bf8ec2e7
Efanov P.A <efanov.pa.a@gmail.com>
2020-08-31 00:03:29 +0300

    Добавлены модели статьи и описания

---
+++ AyanaWebApi/Models/TegreiDescription.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AyanaWebApi.Models
+{
+    /// <summary>
+    /// Основная информация о сайте
+    /// </summary>
+    public class TegreiDescription
+    {
+        /// <summary>
+        /// Название сайта, которое будет отображаться в заголовке и подвале
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Контактный email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Краткий дисклеймер
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
