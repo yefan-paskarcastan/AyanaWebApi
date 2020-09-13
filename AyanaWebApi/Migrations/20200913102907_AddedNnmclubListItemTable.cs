e034314486bb260ff424ccd203e87a901a297cf9
Efanov P.A <efanov.pa.a@gmail.com>
2020-09-13 13:44:02 +0300

    Миграция для таблицы списка клуба

---
+++ AyanaWebApi/Migrations/20200913102907_AddedNnmclubListItemTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AyanaWebApi.Migrations
+{
+    public partial class AddedNnmclubListItemTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NnmclubListItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Added = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Href = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NnmclubListItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NnmclubListItems");
+        }
+    }
+}
